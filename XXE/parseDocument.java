80b9545144ff1f1f290e5c401cbf6fc079a8fdf4
Shreyansh Kansara <71801154+shreyanshkansara20@users.noreply.github.com>
2020-11-03 02:16:29 -0500

    Add files via upload

---
+++ XXE/parseDocument.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.InputSource;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class parseDocument {
+ public static void main(String[] args) {
+  try {
+   DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+   DocumentBuilder builder = factory.newDocumentBuilder();
+   Document doc = builder.parse(new InputSource("contacts.xml"));
+   NodeList nodeList = doc.getElementsByTagName("contact");
+   for (int s = 0; s < nodeList.getLength(); s++) {
+     Node firstNode = nodeList.item(s);
+     if (firstNode.getNodeType() == Node.ELEMENT_NODE) {
+       Element firstElement = (Element) firstNode;
+       NodeList firstNameElementList = firstElement.getElementsByTagName("firstname");
+       Element firstNameElement = (Element) firstNameElementList.item(0);
+       NodeList firstName = firstNameElement.getChildNodes();
+       System.out.println("First Name: "  + ((Node) firstName.item(0)).getNodeValue());
+       NodeList lastNameElementList = firstElement.getElementsByTagName("lastname");
+       Element lastNameElement = (Element) lastNameElementList.item(0);
+       NodeList lastName = lastNameElement.getChildNodes();
+       System.out.println("Last Name: " + ((Node) lastName.item(0)).getNodeValue());
+     }
+    }
+  } catch (Exception e) {
+    e.printStackTrace();
+  }
+ }
+}
+
